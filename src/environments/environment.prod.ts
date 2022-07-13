87e5eb9f05931bc1666942f85a483bccf0df08d1
minhtri188 <102676218+minhtri188@users.noreply.github.com>
2022-07-13 16:27:32 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://aa5a81bb83a5646d4a8b22c6ca75da0d-420815355.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
